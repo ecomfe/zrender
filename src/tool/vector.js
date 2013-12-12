44c154de043c988c17997e04d244ffb6f45b284a
lang <bm2736892@gmail.com>
2013-12-12 14:02:42 +0800

    vector优化，加入mergeFast（舍去了merge中很多判断

--- src/tool/vector.js
+++ src/tool/vector.js
@@ -2,38 +2,60 @@
  * zrender: 向量操作类
  *
  * author : lang(shenyi01@baidu.com)
- * code from vec2 in http://glmatrix.net/
  */
 define(
     function() {
-       var vector = {
+        var ArrayCtor
+            = typeof Float32Array === 'undefined'
+            ? Array
+            : Float32Array;
+        var vector = {
+            create : function(x, y) {
+                var out = new ArrayCtor(2);
+                out[0] = x || 0;
+                out[1] = y || 0;
+                return out;
+            },
+            copy : function(out, v) {
+                out[0] = v[0];
+                out[1] = v[1];
+            },
+            set : function(out, a, b) {
+                out[0] = a;
+                out[1] = b;
+            },
             add : function(out, v1, v2) {
-                out[0] = v1[0]+v2[0];
-                out[1] = v1[1]+v2[1];
+                out[0] = v1[0] + v2[0];
+                out[1] = v1[1] + v2[1];
+                return out;
+            },
+            scaleAndAdd : function(out, v1, v2, a) {
+                out[0] = v1[0] + v2[0] * a;
+                out[1] = v1[1] + v2[1] * a;
                 return out;
             },
             sub : function(out, v1, v2) {
-                out[0] = v1[0]-v2[0];
-                out[1] = v1[1]-v2[1];
+                out[0] = v1[0] - v2[0];
+                out[1] = v1[1] - v2[1];
                 return out;
             },
             length : function(v) {
-                return Math.sqrt( this.lengthSquare(v) );
+                return Math.sqrt(this.lengthSquare(v));
             },
             lengthSquare : function(v) {
-                return v[0]*v[0]+v[1]*v[1];
+                return v[0] * v[0] + v[1] * v[1];
             },
             mul : function(out, v1, v2) {
-                out[0] = v1[0]*v2[0];
-                out[1] = v1[1]*v2[1];
+                out[0] = v1[0] * v2[0];
+                out[1] = v1[1] * v2[1];
                 return out;
             },
             dot : function(v1, v2) {
-                return v1[0]*v2[0]+v1[1]*v2[1];
+                return v1[0] * v2[0] + v1[1] * v2[1];
             },
             scale : function(out, v, s) {
-                out[0] = v[0]*s;
-                out[1] = v[1]*s;
+                out[0] = v[0] * s;
+                out[1] = v[1] * s;
                 return out;
             },
             normalize : function(out, v) {
@@ -58,8 +80,8 @@ define(
                 out[1] = -v[1];
             },
             middle : function(out, v1, v2) {
-                out[0] = (v1[0]+v2[0])/2;
-                out[1] = (v1[1]+v2[1])/2;
+                out[0] = (v1[0] + v2[0])/2;
+                out[1] = (v1[1] + v2[1])/2;
                 return out;
             }
         };
