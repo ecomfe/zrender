bbef2d5c1060003a0f0ba1b0076efdf40f6646f3
lang <bm2736892@gmail.com>
2014-07-15 08:19:26 +0800

    Line dash 优化

--- src/shape/util/dashedLineTo.js
+++ src/shape/util/dashedLineTo.js
@@ -7,25 +7,42 @@
 
 define(
     function (/* require */) {
+
+        var dashPattern = [5, 5];
         /**
          * 虚线lineTo 
          */
         return function (ctx, x1, y1, x2, y2, dashLength) {
+            // http://msdn.microsoft.com/en-us/library/ie/dn265063(v=vs.85).aspx
+            if (ctx.setLineDash) {
+                dashPattern[0] = dashPattern[1] = dashLength;
+                ctx.setLineDash(dashPattern)
+                ctx.moveTo(x1, y1);
+                ctx.lineTo(x2, y2);
+                return;
+            }
+
             dashLength = typeof dashLength != 'number'
                             ? 5 
                             : dashLength;
 
-            var deltaX = x2 - x1;
-            var deltaY = y2 - y1;
+            var dx = x2 - x1;
+            var dy = y2 - y1;
             var numDashes = Math.floor(
-                Math.sqrt(deltaX * deltaX + deltaY * deltaY) / dashLength
+                Math.sqrt(dx * dx + dy * dy) / dashLength
             );
-
+            dx = dx / numDashes;
+            dy = dy / numDashes;
+            var flag = true;
             for (var i = 0; i < numDashes; ++i) {
-                ctx[i % 2 ? 'lineTo' : 'moveTo'](
-                    x1 + (deltaX / numDashes) * i,
-                    y1 + (deltaY / numDashes) * i
-                );
+                if (flag) {
+                    ctx.lineTo(x1, y1);
+                } else {
+                    ctx.moveTo(x1, y1);
+                }
+                flag = !flag;
+                x1 += dx;
+                y1 += dy;
             }
             ctx.lineTo(x2, y2);
         };
