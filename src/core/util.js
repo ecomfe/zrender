ad4d5189ed0211c248dd1cfeb75c38f2e23f16d7
pah100 <sushuang0322@gmail.com>
2015-11-05 15:23:49 +0800

    fix merge

--- src/core/util.js
+++ src/core/util.js
@@ -85,7 +85,7 @@ define(function(require) {
                 else if (overwrite || !(key in target)) {
                     // 否则只处理overwrite为true，或者在目标对象中没有此属性的情况
                     // NOTE，在 target[key] 不存在的时候也是直接覆盖
-                    target[key] = clone(source[key]);
+                    target[key] = clone(source[key], true);
                 }
             }
         }
