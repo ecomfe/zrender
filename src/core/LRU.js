ce291537d0aed6988d53d6670b83c74aa68fa010
lang <bm2736892@gmail.com>
2015-11-29 16:26:16 +0800

    LRU tweak

--- src/core/LRU.js
+++ src/core/LRU.js
@@ -18,7 +18,7 @@ define(function(require) {
          */
         this.tail = null;
 
-        this._length = 0;
+        this._len = 0;
     };
 
     var linkedListProto = LinkedList.prototype;
@@ -40,12 +40,13 @@ define(function(require) {
     linkedListProto.insertEntry = function(entry) {
         if (!this.head) {
             this.head = this.tail = entry;
-        } else {
+        }
+        else {
             this.tail.next = entry;
             entry.prev = this.tail;
             this.tail = entry;
         }
-        this._length++;
+        this._len++;
     };
 
     /**
@@ -57,25 +58,27 @@ define(function(require) {
         var next = entry.next;
         if (prev) {
             prev.next = next;
-        } else {
+        }
+        else {
             // Is head
             this.head = next;
         }
         if (next) {
             next.prev = prev;
-        } else {
+        }
+        else {
             // Is tail
             this.tail = prev;
         }
         entry.next = entry.prev = null;
-        this._length--;
+        this._len--;
     };
 
     /**
      * @return {number}
      */
-    linkedListProto.length = function() {
-        return this._length;
+    linkedListProto.len = function() {
+        return this._len;
     };
 
     /**
@@ -123,7 +126,7 @@ define(function(require) {
         var list = this._list;
         var map = this._map;
         if (map[key] == null) {
-            var len = list.length();
+            var len = list.len();
             if (len >= this._maxSize && len > 0) {
                 // Remove the least recently used
                 var leastUsedEntry = list.head;
@@ -144,7 +147,7 @@ define(function(require) {
     LRUProto.get = function(key) {
         var entry = this._map[key];
         var list = this._list;
-        if (typeof(entry) != 'undefined') {
+        if (entry != null) {
             // Put the latest used entry in the tail
             if (entry !== list.tail) {
                 list.remove(entry);
@@ -152,18 +155,6 @@ define(function(require) {
             }
 
             return entry.value;
-        }
-    };
-
-    /**
-     * @param {string} key
-     */
-    LRUProto.remove = function(key) {
-        var map = this._map;
-        var entry = map[key];
-        if (entry != null) {
-            delete map[key];
-            this._list.remove(entry);
         }
     };
 
