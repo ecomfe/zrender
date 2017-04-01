203decc9e2366d192d092a600a817f5f161a361b
pissang <bm2736892@gmail.com>
2017-04-01 21:31:21 +0800

    text baseline tweak

--- src/contain/text.js
+++ src/contain/text.js
@@ -82,7 +82,8 @@ define(function (require) {
 
         var textHeight = textRect.height;
 
-        var halfHeight = height / 2 - textHeight / 2;
+        var lineHeight = textRect.lineHeight;
+        var halfHeight = height / 2 - textHeight / 2 + lineHeight;
 
         var textAlign = 'left';
 
@@ -99,12 +100,12 @@ define(function (require) {
                 break;
             case 'top':
                 x += width / 2;
-                y -= distance + textHeight;
+                y -= distance + textHeight - lineHeight;
                 textAlign = 'center';
                 break;
             case 'bottom':
                 x += width / 2;
-                y += height + distance;
+                y += height + distance + lineHeight;
                 textAlign = 'center';
                 break;
             case 'inside':
@@ -129,7 +130,7 @@ define(function (require) {
                 break;
             case 'insideBottom':
                 x += width / 2;
-                y += height - textHeight - distance;
+                y += height - textHeight - distance + lineHeight;
                 textAlign = 'center';
                 break;
             case 'insideTopLeft':
@@ -144,11 +145,11 @@ define(function (require) {
                 break;
             case 'insideBottomLeft':
                 x += distance;
-                y += height - textHeight - distance;
+                y += height - textHeight - distance + lineHeight;
                 break;
             case 'insideBottomRight':
                 x += width - distance;
-                y += height - textHeight - distance;
+                y += height - textHeight - distance + lineHeight;
                 textAlign = 'right';
                 break;
         }
@@ -157,7 +158,7 @@ define(function (require) {
             x: x,
             y: y,
             textAlign: textAlign,
-            textBaseline: 'top'
+            textBaseline: 'alphabetic'
         };
     }
 
