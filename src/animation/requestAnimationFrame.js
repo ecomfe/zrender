1d37e6c665663d6cf567b13f2f2849a0bb91e1f0
tony <cn.tony@outlook.com>
2017-04-14 08:58:53 +0800

    Update requestAnimationFrame.js
    repair use echarts and vuejs in IE&Edge,report TypeError: Invalid calling object

--- src/animation/requestAnimationFrame.js
+++ src/animation/requestAnimationFrame.js
@@ -1,6 +1,6 @@
 define(function(require) {
 
-    return (typeof window !== 'undefined' &&
+    var requestAnimationFrame= (typeof window !== 'undefined' &&
                                     (window.requestAnimationFrame
                                     || window.msRequestAnimationFrame
                                     || window.mozRequestAnimationFrame
@@ -8,4 +8,8 @@ define(function(require) {
                                 || function (func) {
                                     setTimeout(func, 16);
                                 };
+    //当在Vuejs及IE环境下初始化时报TypeCall Error.
+    return function(fn){
+        requestAnimationFrame.call(window,fn);
+    };
 });
