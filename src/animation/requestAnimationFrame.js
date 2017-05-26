1b94b6a6181dfd22bf9837010c0d4ed6f358686e
pissang <bm2736892@gmail.com>
2017-05-26 17:38:27 +0800

    Fix requestAnimationFrame lost context in eval. #189

--- src/animation/requestAnimationFrame.js
+++ src/animation/requestAnimationFrame.js
@@ -1,11 +1,13 @@
 define(function(require) {
 
     return (typeof window !== 'undefined' &&
-                                    (window.requestAnimationFrame
-                                    || window.msRequestAnimationFrame
-                                    || window.mozRequestAnimationFrame
-                                    || window.webkitRequestAnimationFrame))
-                                || function (func) {
-                                    setTimeout(func, 16);
-                                };
+                ((window.requestAnimationFrame && window.requestAnimationFrame.bind(window))
+                // https://github.com/ecomfe/zrender/issues/189#issuecomment-224919809
+                || (window.msRequestAnimationFrame && window.msRequestAnimationFrame.bind(window))
+                || window.mozRequestAnimationFrame
+                || window.webkitRequestAnimationFrame)
+            )
+            || function (func) {
+                setTimeout(func, 16);
+            };
 });
