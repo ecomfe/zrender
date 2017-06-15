9f9c50d447e24852d513e384ba6914baefadfe3c
pah100 <sushuang0322@gmail.com>
2017-06-15 15:51:16 +0800

    release 3.5.2

--- src/zrender.js
+++ src/zrender.js
@@ -31,7 +31,7 @@ define(function(require) {
     /**
      * @type {string}
      */
-    zrender.version = '3.5.1';
+    zrender.version = '3.5.2';
 
     /**
      * Initializing a zrender instance
