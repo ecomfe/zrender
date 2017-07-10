8205675f7693552e3cf64a13dfdb34a34ba60598
pah100 <sushuang0322@gmail.com>
2017-07-10 17:15:02 +0800

    tweak for #5777

--- src/graphic/shape/Sector.js
+++ src/graphic/shape/Sector.js
@@ -5,15 +5,8 @@
 
 define(function (require) {
 
-    var env = require('../../core/env');
     var Path = require('../Path');
-
-    var shadowTemp = [
-        ['shadowBlur', 0],
-        ['shadowColor', '#000'],
-        ['shadowOffsetX', 0],
-        ['shadowOffsetY', 0]
-    ];
+    var fixClipWithShadow = require('../helper/fixClipWithShadow');
 
     return Path.extend({
 
@@ -36,48 +29,7 @@ define(function (require) {
             clockwise: true
         },
 
-        brush: (env.browser.ie && env.browser.version >= 11) // version: '11.0'
-            // Fix weird bug in some version of IE11 (like 11.0.9600.17801),
-            // where exception "unexpected call to method or property access"
-            // might be thrown when calling ctx.fill after a path whose area size
-            // is zero is drawn and ctx.clip() is called and shadowBlur is set.
-            // (e.g.,
-            //  ctx.moveTo(10, 10);
-            //  ctx.lineTo(20, 10);
-            //  ctx.closePath();
-            //  ctx.clip();
-            //  ctx.shadowBlur = 10;
-            //  ...
-            //  ctx.fill();
-            // )
-            ? function () {
-                var clipPaths = this.__clipPaths;
-                var style = this.style;
-                var modified;
-
-                if (clipPaths) {
-                    for (var i = 0; i < clipPaths.length; i++) {
-                        var shape = clipPaths[i] && clipPaths[i].shape;
-                        if (shape && shape.startAngle === shape.endAngle) {
-                            for (var j = 0; j < shadowTemp.length; j++) {
-                                shadowTemp[j][2] = style[shadowTemp[j][0]];
-                                style[shadowTemp[j][0]] = shadowTemp[j][1];
-                            }
-                            modified = true;
-                            break;
-                        }
-                    }
-                }
-
-                Path.prototype.brush.apply(this, arguments);
-
-                if (modified) {
-                    for (var j = 0; j < shadowTemp.length; j++) {
-                        style[shadowTemp[j][0]] = shadowTemp[j][2];
-                    }
-                }
-            }
-            : Path.prototype.brush,
+        brush: fixClipWithShadow(Path.prototype.brush),
 
         buildPath: function (ctx, shape) {
 
