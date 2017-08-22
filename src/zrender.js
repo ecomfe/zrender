aa65bf6d1b4318e720e500f25bfd2c98d90135d3
sushuang <sushuang0322@gmail.com>
2017-08-22 12:09:20 +0800

    release 3.6.0

--- src/zrender.js
+++ src/zrender.js
@@ -31,7 +31,7 @@ define(function (require) {
     /**
      * @type {string}
      */
-    zrender.version = '3.5.2';
+    zrender.version = '3.6.0';
 
     /**
      * Initializing a zrender instance
