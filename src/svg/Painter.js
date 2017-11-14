ecomfe/zrender

commit d07e0e099c8e89c9584c6ef6a9fe6b2b66b5f383

fix: svg bug when roaming with bmap

src/svg/Painter.js
@@ -96,12 +96,13 @@ var SVGPainter = function (root, storage, opts) {
     svgRoot.setAttribute('version', '1.1');
     svgRoot.setAttribute('baseProfile', 'full');
     svgRoot.style['user-select'] = 'none';
+    svgRoot.style.cssText = 'position:absolute;left:0;top:0;';
 
     this.gradientManager = new GradientManager(svgRoot);
     this.clipPathManager = new ClippathManager(svgRoot);
 
     var viewport = document.createElement('div');
-    viewport.style.cssText = 'overflow: hidden;';
+    viewport.style.cssText = 'overflow:hidden;position:relative';
 
     this._svgRoot = svgRoot;
     this._viewport = viewport;

commit 293d3948a1952d6a1c5e2a8dc057c28e5ed62a8b

fix(svg): user-select problem caused from d07e0e099c8e89c9584c6ef6a9fe6b2b66b5f383

src/svg/Painter.js
@@ -95,8 +95,7 @@ var SVGPainter = function (root, storage, opts) {
     svgRoot.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
     svgRoot.setAttribute('version', '1.1');
     svgRoot.setAttribute('baseProfile', 'full');
-    svgRoot.style['user-select'] = 'none';
-    svgRoot.style.cssText = 'position:absolute;left:0;top:0;';
+    svgRoot.style.cssText = 'user-select:none;position:absolute;left:0;top:0;';
 
     this.gradientManager = new GradientManager(svgRoot);
     this.clipPathManager = new ClippathManager(svgRoot);