532483fd185f3c3455b6fd986a809bc51e7c9a69
sushuang <sushuang0322@gmail.com>
2017-12-27 21:17:01 +0800

    Fix undefined problem.

--- src/contain/text.js
+++ src/contain/text.js
@@ -671,11 +671,12 @@ function pushTokens(block, str, styleName) {
 export function makeFont(style) {
     // FIXME in node-canvas fontWeight is before fontStyle
     // Use `fontSize` `fontFamily` to check whether font properties are defined.
-    return trim((style.fontSize || style.fontFamily) && [
+    var font = (style.fontSize || style.fontFamily) && [
         style.fontStyle,
         style.fontWeight,
         (style.fontSize || 12) + 'px',
         // If font properties are defined, `fontFamily` should not be ignored.
         style.fontFamily || 'sans-serif'
-    ].join(' ')) || style.textFont || style.font;
+    ].join(' ');
+    return font && trim(font) || style.textFont || style.font;
 }
