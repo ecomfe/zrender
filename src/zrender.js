6a3f4fc27973794b222601e7e3c986b37b6241a6
sushuang <sushuang0322@gmail.com>
2019-04-21 15:08:30 +0800

    Fix that refresh hover called twice.

--- src/zrender.js
+++ src/zrender.js
@@ -227,14 +227,14 @@ ZRender.prototype = {
      */
     refreshImmediately: function () {
         // var start = new Date();
+
         // Clear needsRefresh ahead to avoid something wrong happens in refresh
         // Or it will cause zrender refreshes again and again.
-        this._needsRefresh = false;
+        this._needsRefresh = this._needsRefreshHover = false;
         this.painter.refresh();
-        /**
-         * Avoid trigger zr.refresh in Element#beforeUpdate hook
-         */
-        this._needsRefresh = false;
+        // Avoid trigger zr.refresh in Element#beforeUpdate hook
+        this._needsRefresh = this._needsRefreshHover = false;
+        
         // var end = new Date();
         // var log = document.getElementById('log');
         // if (log) {
