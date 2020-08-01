199b983bed7131b66b8eaa99abdbf132f1200efa
plainheart <yhen@all-my-life.cn>
2020-08-01 15:20:47 +0800

    fix: fix code style flaw.

--- src/animation/requestAnimationFrame.ts
+++ src/animation/requestAnimationFrame.ts
@@ -5,11 +5,11 @@ let requestAnimationFrame: RequestAnimationFrameType;
 requestAnimationFrame = (
 	typeof window !== 'undefined'
 		&& (window.requestAnimationFrame && window.requestAnimationFrame.bind(window))
-    	// https://github.com/ecomfe/zrender/issues/189#issuecomment-224919809
-    	|| ((window as any).msRequestAnimationFrame && (window as any).msRequestAnimationFrame.bind(window))
-   	 	|| (window as any).mozRequestAnimationFrame
-    	|| window.webkitRequestAnimationFrame)
-	|| function (func: Parameters<RequestAnimationFrameType>[0]): number {
+		// https://github.com/ecomfe/zrender/issues/189#issuecomment-224919809
+		|| ((window as any).msRequestAnimationFrame && (window as any).msRequestAnimationFrame.bind(window))
+		|| (window as any).mozRequestAnimationFrame
+		|| window.webkitRequestAnimationFrame
+	) || function (func: Parameters<RequestAnimationFrameType>[0]): number {
 	    return setTimeout(func, 16) as any;
 	};
 
