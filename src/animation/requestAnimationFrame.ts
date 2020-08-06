1a8dcbbf0043d5a3eb17a71cb985a7b1f0b4f0da
Jian Zhang <dsonet@msn.com>
2020-08-06 13:27:21 -0700

    fix window !== 'undefined' branch ends too early

--- src/animation/requestAnimationFrame.ts
+++ src/animation/requestAnimationFrame.ts
@@ -4,12 +4,12 @@ let requestAnimationFrame: RequestAnimationFrameType;
 
 requestAnimationFrame = (
 	typeof window !== 'undefined'
-		&& (window.requestAnimationFrame && window.requestAnimationFrame.bind(window))
+		&& ((window.requestAnimationFrame && window.requestAnimationFrame.bind(window))
 		// https://github.com/ecomfe/zrender/issues/189#issuecomment-224919809
 		|| ((window as any).msRequestAnimationFrame && (window as any).msRequestAnimationFrame.bind(window))
 		|| (window as any).mozRequestAnimationFrame
 		|| window.webkitRequestAnimationFrame
-	) || function (func: Parameters<RequestAnimationFrameType>[0]): number {
+	)) || function (func: Parameters<RequestAnimationFrameType>[0]): number {
 	    return setTimeout(func, 16) as any;
 	};
 
