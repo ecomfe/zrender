a3e315ea6afa796204cef0d5073012ca285c44a1
plainheart <yhen@all-my-life.cn>
2020-10-16 00:29:33 +0800

    fix(test): fix wrong value in tool/color test case.

--- test/ut/spec/tool/color.test.ts
+++ test/ut/spec/tool/color.test.ts
@@ -2,8 +2,8 @@ import * as colorTool from '../../../../src/tool/color';
 
 describe('colorTool', function () {
 
-	it(`\`rgba(17, 263, 69)\` should be converted to [17, 263, 69, 1]`, function () {
-		expect(colorTool.parse('rgba(17, 263, 69)')).toEqual([17, 263, 69, 1]);
+	it(`\`rgba(17, 163, 69)\` should be converted to [17, 163, 69, 1]`, function () {
+		expect(colorTool.parse('rgba(17, 163, 69)')).toEqual([17, 163, 69, 1]);
 	});
 
 	it(`\`#14c4baff\` should be converted to [20, 196, 186, 1]`, function () {
