e6e5a9b11eade52c90c8dc1eeb84e6a4a141201a
pissang <bm2736892@gmail.com>
2020-10-19 13:28:52 +0800

    test(color): add more test cases for color parse

--- test/ut/spec/tool/color.test.ts
+++ test/ut/spec/tool/color.test.ts
@@ -1,17 +1,21 @@
 import * as colorTool from '../../../../src/tool/color';
+import { color } from '../../../../src/export';
 
 describe('colorTool', function () {
 
-	it(`\`rgba(17, 163, 69)\` should be converted to [17, 163, 69, 1]`, function () {
-		expect(colorTool.parse('rgba(17, 163, 69)')).toEqual([17, 163, 69, 1]);
+	([
+		['rgba(17, 163, 69)', [17, 163, 69, 1]],
+		['rgba(17, 163, 69, 0.5)', [17, 163, 69, 0.5]],
+		['rgb(17, 163, 69)', [17, 163, 69, 1]],
+		['#14c4ba00', [20, 196, 186, 0]],
+		['#14c4ba', [20, 196, 186, 1]],
+		['#07f0', [0, 119, 255, 0]],
+		['#07f', [0, 119, 255, 1]],
+		['red', [255, 0, 0, 1]],
+		['blue', [0, 0, 255, 1]]
+	] as [string, number[]][]).forEach(([colorStr, colorRes]) => {
+		it(`'${colorStr}' should be converted to [${colorRes.join(', ')}]`, function () {
+			expect(colorTool.parse(colorStr)).toEqual(colorRes);
+		});
 	});
-
-	it(`\`#14c4baff\` should be converted to [20, 196, 186, 1]`, function () {
-		expect(colorTool.parse('#14c4baff')).toEqual([20, 196, 186, 1]);
-	});
-
-	it(`\`#07f0\` should be converted to [0, 119, 255, 0]`, function () {
-		expect(colorTool.parse('#07f0')).toEqual([0, 119, 255, 0]);
-	});
-
 });
