a0a035eac41562662febb1499b1ea3a00264ae49
100pah <sushuang0322@gmail.com>
2021-04-12 16:11:45 +0800

    release 5.1.0

--- src/zrender.ts
+++ src/zrender.ts
@@ -520,7 +520,7 @@ export function registerPainter(name: string, Ctor: PainterBaseCtor) {
 /**
  * @type {string}
  */
-export const version = '5.0.4';
+export const version = '5.1.0';
 
 
 export interface ZRenderType extends ZRender {};
