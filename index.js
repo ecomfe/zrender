b0ede2ca0a8e22763c7a7add0157642b37d658dc
pissang <bm2736892@gmail.com>
2021-10-08 10:24:36 +0800

    fix entry js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 export * from './lib/zrender';
 export * from './lib/export';
 
-import {registerPainter} from './src/zrender';
-import CanvasPainter from './src/canvas/Painter';
-import SVGPainter from './src/svg/Painter';
+import {registerPainter} from './lib/zrender';
+import CanvasPainter from './lib/canvas/Painter';
+import SVGPainter from './lib/svg/Painter';
 registerPainter('canvas', CanvasPainter);
 registerPainter('svg', SVGPainter);
