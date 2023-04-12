0a1426ff495f263cc097057b7206aff866a8eba8
Wetterquarz <cedric.lippke@hotmail.de>
2023-04-12 16:49:13 +0200

    Made non-BMP characters be treated as a single character for wrapping.

--- src/graphic/helper/parseText.ts
+++ src/graphic/helper/parseText.ts
@@ -636,8 +636,8 @@ function wrapText(
     let accumWidth = 0;
 
     for (let i = 0; i < text.length; i++) {
-
-        const ch = text.charAt(i);
+        let ch;
+        [ch, i] = getWholeCharAndI(text, i);
         if (ch === '\n') {
             if (currentWord) {
                 line += currentWord;
@@ -761,4 +761,45 @@ function wrapText(
         lines: lines,
         linesWidths
     };
+}
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt#getting_whole_characters
+function getWholeCharAndI(str: string, i: number): [string, number] {
+    const code = str.charCodeAt(i);
+  
+    if (isNaN(code)) {
+        return ["", i]; // Position not found
+    }
+    if (code < 0xd800 || code > 0xdfff) {
+        return [str.charAt(i), i]; // Normal character, keeping 'i' the same
+    }
+  
+    // High surrogate (could change last hex to 0xDB7F to treat high private
+    // surrogates as single characters)
+    if (0xd800 <= code && code <= 0xdbff) {
+        if (str.length <= i + 1) {
+            throw new Error("High surrogate without following low surrogate");
+        }
+        const next = str.charCodeAt(i + 1);
+        if (next < 0xdc00 || next > 0xdfff) {
+            throw new Error("High surrogate without following low surrogate");
+        }
+        return [str.charAt(i) + str.charAt(i + 1), i + 1];
+    }
+  
+    // Low surrogate (0xDC00 <= code && code <= 0xDFFF)
+    if (i === 0) {
+        throw new Error("Low surrogate without preceding high surrogate");
+    }
+  
+    const prev = str.charCodeAt(i - 1);
+  
+    // (could change last hex to 0xDB7F to treat high private surrogates
+    // as single characters)
+    if (prev < 0xd800 || prev > 0xdbff) {
+        throw new Error("Low surrogate without preceding high surrogate");
+    }
+  
+    // Return the next character instead (and increment)
+    return [str.charAt(i + 1), i + 1];
 }
